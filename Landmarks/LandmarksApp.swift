630cb3725d257fd9c97ae272625f795a52d6e8e1
Joseph Riddle <joeriddles10@gmail.com>
2022-06-12 18:01:16 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Joseph Riddle on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
